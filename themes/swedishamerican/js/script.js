c148a8823baa6c50df3324d4c3dd0046fbbf834a
Tim Grable <tgrable@trekk.com>
2017-07-05 13:15:00 -0500

    script js file

--- themes/swedishamerican/js/script.js
+++ themes/swedishamerican/js/script.js
@@ -3,35 +3,55 @@
 
   Drupal.behaviors.swedishamerican = {
     attach: function(context, settings) {
-			$('.markup-area').detach().appendTo('#swedishamerican-providers-form');
 			
-      $('.facebook-share').on('click', function(e) {
-				var tag = $(e.target).parent().parent().parent().parent(),
-				sib = tag.siblings().children(),
-				url = sib.find('img').attr('src');
+			var mainController = {
+				
+				pathname: window.location.pathname, // Returns path only
+				url: window.location.href,    // Returns full URL
+				
+				init: function() {
+					if (this.pathname === '/') {
+						$('header').css("background-image", "url(/themes/swedishamerican/images/img-HdrHome-01.png)");
+						$('header').css("height", "615px");
+					}
+					else if (this.pathname.includes("providers")) {
+						$('.markup-area').detach().appendTo('#swedishamerican-providers-form');
 
-				var urlRoot = window.location.origin;
-				var finalURL = urlRoot + '/' + $(this).data("href");
+						$('.facebook-share').on('click', function(e) {
+							var tag = $(e.target).parent().parent().parent().parent(),
+							sib = tag.siblings().children(),
+							url = sib.find('img').attr('src');
 
-				var winTop = (screen.height / 2) - (500 / 2);
-        		var winLeft = (screen.width / 2) - (1150 / 2);
-        		window.open('https://www.facebook.com/sharer/sharer.php?u=' + finalURL, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + 575 + ',height=' + 250);
-			});
+							var urlRoot = window.location.origin;
+							var finalURL = urlRoot + '/' + $(this).data("href");
 
-      $('.twitter-share').on('click', function(e) {
-				var urlRoot = window.location.origin;
-				var title = $(this).data("name");
-				var path = $(this).data("href");
-				window.open("https://twitter.com/share?url=" + urlRoot + path + "&text=Check out " + title + " at Swedish American Hospital", "_blank", "width=575, height=250");
-			});
+							var winTop = (screen.height / 2) - (500 / 2);
+							var winLeft = (screen.width / 2) - (1150 / 2);
+							window.open('https://www.facebook.com/sharer/sharer.php?u=' + finalURL, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + 575 + ',height=' + 250);
+						});
 
-      // Need to Fix
-      $('.linkedin-share').on('click', function(e) {
-				var urlRoot = window.location.origin;
-				var title = $(this).data("name");
-				var path = $(this).data("href");
-				window.open("http://www.linkedin.com/shareArticle?mini=true&url=" + urlRoot + path + "&title=" + title, "_blank", "width=575, height=250"); 
-			});
+						$('.twitter-share').on('click', function(e) {
+							var urlRoot = window.location.origin;
+							var title = $(this).data("name");
+							var path = $(this).data("href");
+							window.open("https://twitter.com/share?url=" + urlRoot + path + "&text=Check out " + title + " at Swedish American Hospital", "_blank", "width=575, height=250");
+						});
+
+						// Need to Fix
+						$('.linkedin-share').on('click', function(e) {
+							var urlRoot = window.location.origin;
+							var title = $(this).data("name");
+							var path = $(this).data("href");
+							window.open("http://www.linkedin.com/shareArticle?mini=true&url=" + urlRoot + path + "&title=" + title, "_blank", "width=575, height=250"); 
+						});
+					}
+					else {
+						console.log('Else');
+					}
+				}
+			};
+			mainController.init();
+
     }
   };
 
